1279c2644ab7ffcbdc14d034e251fd89a3ac4d09
quay0 <83027933+quay0@users.noreply.github.com>
2021-10-02 17:06:20 +0100

    Update chess.py

--- source/chess.py
+++ source/chess.py
@@ -35,14 +35,25 @@ light = (255, 255, 255)
 dark = (171, 122, 101)
 
 #Setting starting poition
-position = [[br,bn,bb,bq,bk,bb,bn,br],
-		 [bp,bp,bp,bp,bp,bp,bp,bp],
+#Unlike on a normal chess board (where it would be 1-8), this records positions as 0-7
+
+#position = [[br,bn,bb,bq,bk,bb,bn,br]
+#		 [bp,bp,bp,bp,bp,bp,bp,bp],
+#		 [1,1,1,1,1,1,1,1],
+#		 [1,1,1,1,1,1,1,1],
+#		 [1,1,1,1,1,1,1,1],
+#		 [1,1,1,1,1,1,1,1],
+#		 [wp,wp,wp,wp,wp,wp,wp,wp],
+#		 [wr,wn,wb,wq,wk,wb,wn,wr]]
+
+position = [[br,1,1,1,1,1,1,1],
+		 [1,1,1,1,1,1,1,wq],
+		 [1,1,bb,1,1,1,1,1],
 		 [1,1,1,1,1,1,1,1],
+		 [1,1,1,1,1,1,wb,1],
 		 [1,1,1,1,1,1,1,1],
-		 [1,1,1,1,1,1,1,1],
-		 [1,1,1,1,1,1,1,1],
-		 [wp,wp,wp,wp,wp,wp,wp,wp],
-		 [wr,wn,wb,wq,wk,wb,wn,wr]]
+		 [1,1,1,bq,1,1,1,1],
+		 [1,1,1,1,1,1,1,wr]]
 
 game_display = pygame.display.set_mode((512+128,512+128))
 boardLength = 8
@@ -100,6 +111,8 @@ def draw_turn():
 	label = font.render("0Chess", 1, (0,0,0))
 	window.blit(label, (512, 32))
 
+legal = False
+
 #main
 loop = True
 while loop:
@@ -114,29 +127,75 @@ while loop:
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			if selected == False:
 				if skip == False:
-					my, mx = pygame.mouse.get_pos()
+					my, mx = pygame.mouse.get_pos() #mx and my are the starting square coordinates
 					mx = mx/64 - 1
 					my = my/64 - 1
 					mx = int(mx)
 					my = int(my)
-					if position[mx][my] != 1:
-						current = position[mx][my]
-						selected = True
+					if mx > -1 and mx < 8 and my > -1 and my > 8:
+						if position[mx][my] != 1:
+							current = position[mx][my]
+							selected = True
 					else:
 						skip = True
 				else:
 					skip = False
 			else:
-				new_my, new_mx = pygame.mouse.get_pos()
+				new_my, new_mx = pygame.mouse.get_pos() #mx and my are the target square coordinates
 				new_mx = new_mx/64 - 1
 				new_my = new_my/64 - 1
 				new_mx = int(new_mx)
 				new_my = int(new_my)
+
+				if turn == 1:
+					start = "w"
+				else:
+					start = "b"
+
+				#rook = start + "r"
+				#bishop = start + "b"
+				#queen = start + "q"
+
+				#exec("%s = %d" % (rook,2))
+				#exec("%s = %d" % (bishop,2))
+				#exec("%s = %d" % (queen,2))
+
+				#Rook's legal moves
+				if current == wr or br:
+					if mx == new_mx or my == new_my:
+						legal = True
+					else:
+						legal = False
+								
+				#Bishop's legal moves
+				if current == wb or bb:
+					x = abs(new_mx - mx)
+					y = abs(new_my - my)
+					if x == y and x != 0:
+						legal = True
+					else:
+						legal = False
+
+				#Queen's legal moves
+				if current == wq or bq:
+					if mx == new_mx or my == new_my:
+						legal = True
+					else:
+						x = abs(new_mx - mx)
+						y = abs(new_my - my)
+						if x == y and x != 0:
+							legal = True
+						else:
+							legal = False
+						
+				
+				#Changing array to move pieces
 				if new_mx != mx or new_my != my:
-					position[mx][my] = 1
-					position[new_mx][new_my] = current
+					if legal == True:
+						position[mx][my] = 1
+						position[new_mx][new_my] = current
+						turn ^= 1
 				selected = False
-				turn ^= 1
 
 
 	pygame.display.update()
