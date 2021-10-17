0539cc1ba3da7cd39538e330b82202036e1cc2bf
quay0 <83027933+quay0@users.noreply.github.com>
2021-10-17 18:00:33 +0100

    Update chess.py (#4)

--- source/chess.py
+++ source/chess.py
@@ -1,5 +1,10 @@
+#Wombat chess
+#Open source project for more information see https://github.com/quay0/chess/
+#This is a development build meaning it may contain bugs - if you find any go to https://github.com/quay0/chess/issues/new/choose and select bug report
+
+
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" #Hiding the greetings from the pygame comminity message
 
 import pygame
 pygame.init()
@@ -17,20 +22,36 @@ icon = pygame.image.load("wk.ico")
 pygame.display.set_icon(icon)
 
 #Loading images
-wq = pygame.image.load("wq.png")
-wk = pygame.image.load("wk.png")
-wr = pygame.image.load("wr.png")
-wb = pygame.image.load("wb.png")
-wn = pygame.image.load("wn.png")
-wp = pygame.image.load("wp.png")
+_wq = pygame.image.load("wq.png")
+_wk = pygame.image.load("wk.png")
+_wr = pygame.image.load("wr.png")
+_wb = pygame.image.load("wb.png")
+_wn = pygame.image.load("wn.png")
+_wp = pygame.image.load("wp.png")
 
-bq = pygame.image.load("bq.png")
-bk = pygame.image.load("bk.png")
-br = pygame.image.load("br.png")
-bb = pygame.image.load("bb.png")
-bn = pygame.image.load("bn.png")
-bp = pygame.image.load("bp.png")
+_bq = pygame.image.load("bq.png")
+_bk = pygame.image.load("bk.png")
+_br = pygame.image.load("br.png")
+_bb = pygame.image.load("bb.png")
+_bn = pygame.image.load("bn.png")
+_bp = pygame.image.load("bp.png")
 
+#Piece values
+bp = 100
+bn = 280
+bb = 320
+br = 520
+bq = 920
+bk = 60000
+
+wp = -100
+wn = -280
+wb = -320
+wr = -520
+wq = -920
+wk = -60000
+
+#Setting colors for board
 light = (255, 255, 255)
 dark = (171, 122, 101)
 
@@ -45,13 +66,13 @@ dark = (171, 122, 101)
 #		 [1,1,1,1,1,1,1,1],
 #		 [wp,wp,wp,wp,wp,wp,wp,wp],
 #		 [wr,wn,wb,wq,wk,wb,wn,wr]]
-
+#Testing starting position
 position = [[br,1,1,1,1,1,1,1],
-		 [1,1,1,1,1,1,1,wq],
-		 [1,1,bb,1,1,1,1,1],
+		 [1,1,bk,1,1,1,wn,1],
+		 [1,1,bb,1,1,1,bn,1],
 		 [1,1,1,1,1,1,1,1],
 		 [1,1,1,1,1,1,wb,1],
-		 [1,1,1,1,1,1,1,1],
+		 [1,1,1,1,1,1,wk,1],
 		 [1,1,1,bq,1,1,1,1],
 		 [1,1,1,1,1,1,1,wr]]
 
@@ -61,6 +82,7 @@ game_display.fill(light)
 square_size = 64
 
 def draw_board():
+	#drawing squares
 	count = 0
 	for i in range(1,boardLength+1):
 		for z in range(1,boardLength+1):
@@ -78,27 +100,40 @@ def draw_pieces():
 		while column < 8:
 			current = position[column][row]
 			if current != 1:
-				window.blit(current, (((64 * row) + 74), ((64 * column) + 74)))
+				#Drawing pieces
+				if current == bn:
+					window.blit(_bn, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == bb:
+					window.blit(_bb, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == bk:
+					window.blit(_bk, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == bq:
+					window.blit(_bq, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == br:
+					window.blit(_br, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == bp:
+					window.blit(_bp, (((64 * row) + 74), ((64 * column) + 74)))
+
+				if current == wn:
+					window.blit(_wn, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == wb:
+					window.blit(_wb, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == wk:
+					window.blit(_wk, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == wq:
+					window.blit(_wq, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == wr:
+					window.blit(_wr, (((64 * row) + 74), ((64 * column) + 74)))
+				if current == wp:
+					window.blit(_wp, (((64 * row) + 74), ((64 * column) + 74)))
 			column += 1
 		row += 1
 		column = 0
 
-values = {
-	"Pawn": 10,
-	"Knight": 30,
-	"Bishop": 35,
-	"Rook": 50,
-	"Queen": 90,
-	"King": 9000
-}
-
-
+font = pygame.font.SysFont("consolas", 16)
 turn = 1 #1 for white, 0 for black
-legal = 0 #1 legal, 0 not legal
 selected = False
 skip = False
-
-font = pygame.font.SysFont("consolas", 16)
 
 def draw_turn():
 	pygame.draw.rect(window, (255, 255, 255), (0,0,512+128,64))
@@ -106,6 +141,7 @@ def draw_turn():
 		player = "White"
 	else:
 		player = "Black"
+	#drawing text
 	label = font.render(str(player), 1, (0,0,0))
 	window.blit(label, (64, 32))
 	label = font.render("0Chess", 1, (0,0,0))
@@ -120,9 +156,13 @@ while loop:
 	draw_board()
 	draw_pieces()
 	draw_turn()
+
 	for event in pygame.event.get():
+
+		#closing window
 		if event.type == pygame.QUIT:
-			 loop = False
+			loop = False
+
 		#moving pieces
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			if selected == False:
@@ -147,19 +187,24 @@ while loop:
 				new_mx = int(new_mx)
 				new_my = int(new_my)
 
-				#if turn == 1:
-				#	start = "w"
-				#else:
-				#	start = "b"
-
-				#rook = start + "r"
-				#bishop = start + "b"
-				#queen = start + "q"
-
-				#exec("%s = %d" % (rook,2))
-				#exec("%s = %d" % (bishop,2))
-				#exec("%s = %d" % (queen,2))
-
+				#King's legal moves
+				if current == wk or bk:
+					if mx == new_mx or my == new_my:
+						if mx == x + 1 or mx == x - 1:
+							legal = True
+						elif my == y + 1 or my == y - 1:
+							legal = True
+					else:
+						x = abs(new_mx - mx)
+						y = abs(new_my - my)
+						if x == y and x != 0:
+							if mx == x + 1 or mx == x - 1:
+								legal = True
+							elif my == y + 1 or my == y - 1:
+								legal = True
+						else:
+							legal = False
+				
 				#Rook's legal moves
 				if current == wr or br:
 					if mx == new_mx or my == new_my:
@@ -187,8 +232,20 @@ while loop:
 							legal = True
 						else:
 							legal = False
-						
 				
+				#Knights legal moves
+				if current == wn or bn:
+					if mx != new_mx or my != new_my:
+						x = abs(new_mx - mx)
+						y = abs(new_my - my)
+						if x != y and x == 0:
+							if (mx != x + 2 or mx != x - 2) or (my != y + 2 or my != y - 2):
+								print()
+						else:
+							legal = False
+					else:
+						legal = False
+						
 				#Changing array to move pieces
 				if new_mx != mx or new_my != my:
 					if legal == True:
