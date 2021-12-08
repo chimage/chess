27f721f36936d19cfc16d215732b1187a4e21325
quay0 <83027933+quay0@users.noreply.github.com>
2021-12-08 13:28:19 +0000

    Update chess.py

--- source/chess.py
+++ source/chess.py
@@ -1,4 +1,4 @@
-#Wombat chess
+#Goose chess
 #Open source project - for more information see https://github.com/quay0/chess/
 #This is a development build meaning it may contain bugs - if you find any go to https://github.com/quay0/chess/issues/new/choose and select bug report
 
