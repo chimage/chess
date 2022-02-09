19d7dc9d3bec239c5f4a202dc95323bcb225c402
quay0 <83027933+quay0@users.noreply.github.com>
2022-02-09 13:38:56 +0000

    Add files via upload

---
+++ source/ai.py
@@ -0,0 +1,3 @@
+#Wombat chess
+#Open source project - for more information see https://github.com/quay0/chess/
+#This is a development build meaning it may contain bugs - if you find any go to https://github.com/quay0/chess/issues/new/choose and select bug report
